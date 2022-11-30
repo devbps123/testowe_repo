734ac4fb9cd4a99ce921d76cdb9bf9d1f5ec7977
devbps123 <119406828+devbps123@users.noreply.github.com>
2022-11-30 13:59:27 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,21 +1,13 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
+      source = "hashicorp/google"
+      version = "4.44.1"
     }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 4.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
+    github = {
+      source = "integrations/github"
+      version = "5.10.0"
     }
   }
-  required_version = ">= 0.13"
 }
+
