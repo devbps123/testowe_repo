b5f4c4f7e3932fcb30da76e18cf4f042f4aaeb60
Maciej PIetrzykowski <maciej.pietrzykowski@getindata.com>
2022-11-30 14:10:56 +0100

    x

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "e2-medium"
-  zone         = "europe-central2"
+resource "google_service_account" "service_account" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
+  project = "odnowa4-gid-poc-1930-sandbox"
 }
