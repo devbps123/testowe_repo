efefffd544c2166f3fc238a8c16798ceed56e3b5
devbps123 <119406828+devbps123@users.noreply.github.com>
2022-11-30 13:56:06 +0100

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
